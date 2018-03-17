a25fb10d412cbcf6a015d68358e2c7b8ab7de084
Jaro Reinders <jaro.reinders@gmail.com>
2018-03-18 00:21:57 +0100

    Add basic runNowVty functions and a demo executable

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,5 +1,9 @@
 # Revision history for frpnow-vty
 
+## 0.2.0.0  -- 2018-03-18
+* Added basic 'runNowVty' and 'runNowVtyPure' functions.
+* Added 'demos/Demo.hs' mostly copied from [vty's Demo.hs](https://github.com/jtdaugherty/vty/blob/6f4edc0228660fb407f48c070ba3cee8c7dc7615/demos/Demo.hs).
+
 ## 0.1.0.0  -- 2018-03-17
 
 * First version. Released on an unsuspecting world.
--- src/Control/FRPNow/Vty.hs
+++ src/Control/FRPNow/Vty.hs
@@ -1,4 +1,28 @@
 module Control.FRPNow.Vty where
 
 import Control.FRPNow
-import Graphics.Vty
+import qualified Graphics.Vty as Vty
+import Control.Monad (forever)
+
+type VEvent = Vty.Event
+
+runNowVty
+  :: Vty.Config
+  -> (EvStream VEvent -> Now (BehaviorEnd Vty.Picture a))
+  -> IO a
+runNowVty conf m = do
+  vty <- Vty.mkVty conf
+  runNowMaster $ do
+    (evs, cbk) <- callbackStream
+    async (forever (Vty.nextEvent vty >>= cbk))
+    (b `Until` e) <- m evs
+    sync . Vty.update vty =<< sample b
+    callIOStream (Vty.update vty) (toChanges b)
+    plan ((<$ sync (Vty.shutdown vty)) <$> e)
+
+runNowVtyPure
+  :: Vty.Config
+  -> (EvStream VEvent -> Behavior (BehaviorEnd Vty.Picture a))
+  -> IO a
+runNowVtyPure conf b = runNowVty conf (sample . b)
+
