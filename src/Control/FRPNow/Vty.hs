6263aac0fc1c0b8b57ebb8909fc31c2e1bfb02ab
Jaro Reinders <jaro.reinders@gmail.com>
2018-03-18 12:41:55 +0100

    Add documentation and description

--- src/Control/FRPNow/Vty.hs
+++ src/Control/FRPNow/Vty.hs
@@ -1,14 +1,27 @@
+-- |
+-- Module     : Control.FRPNow.Vty
+-- Copyright  : (c) Jaro Reinders, 2018
+-- License    : GPL-3
+-- Maintainer : jaro.reinders@gmail.com
+--
+-- This module provides interoperability of FRPNow and the vty terminal UI
+-- library.
 module Control.FRPNow.Vty where
 
 import Control.FRPNow
 import qualified Graphics.Vty as Vty
 import Control.Monad (forever)
 
+-- | Alias for 'Vty.Event' to prevent name clash with 'Event'.
 type VEvent = Vty.Event
 
+-- | Run a Now computation which produces a behavior of type Picture, and draw
+-- that on the screen.
 runNowVty
-  :: Vty.Config
+  :: Vty.Config -- ^ The vty configuration to use.
   -> (EvStream VEvent -> Now (BehaviorEnd Vty.Picture a))
+  -- ^ A now computation that takes a stream of vty events and produces
+  -- a behavior of pictures and an ending event.
   -> IO a
 runNowVty conf m = do
   vty <- Vty.mkVty conf
@@ -20,9 +33,12 @@ runNowVty conf m = do
     callIOStream (Vty.update vty) (toChanges b)
     plan ((<$ sync (Vty.shutdown vty)) <$> e)
 
+-- | Like 'runNowVty', but does not allow IO.
 runNowVtyPure
-  :: Vty.Config
+  :: Vty.Config -- ^ The vty configuration to use.
   -> (EvStream VEvent -> Behavior (BehaviorEnd Vty.Picture a))
+  -- ^ A computation that takes a stream of vty events and produces
+  -- a behavior of pictures and an ending event.
   -> IO a
 runNowVtyPure conf b = runNowVty conf (sample . b)
 
